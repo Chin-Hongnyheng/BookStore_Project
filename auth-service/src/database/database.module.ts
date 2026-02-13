60e88409a1af512d1adccaa3ca9d6623b2e95ab4
Chin Hongnyheng <hongnyhengchin@gmail.com>
2026-02-13 12:52:26 +0700

    New Updated

--- auth-service/src/database/database.module.ts
+++ auth-service/src/database/database.module.ts
@@ -1,6 +1,6 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -8,14 +8,19 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (cfg: ConfigService) => ({
-        type: "postgres",
-        host: cfg.get("DB_HOST"),
-        port: Number(cfg.get("DB_PORT")),
-        username: cfg.get("DB_USER"),
-        password: cfg.get("DB_PASSWORD"),
-        database: cfg.get("DB_NAME"),
+        type: 'postgres',
+        host: cfg.get('DB_HOST'),
+        port: Number(cfg.get('DB_PORT')),
+        username: cfg.get('DB_USER'),
+        password: cfg.get('DB_PASSWORD'),
+        database: cfg.get('DB_NAME'),
         autoLoadEntities: true,
         synchronize: true,
+        retryAttempts: 10,
+        retryDelay: 3000,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       }),
     }),
   ],
--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -7,19 +7,24 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ConfigModule,
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (cfg: ConfigService) => ({
+      useFactory: (cfg: ConfigService) => {
+      console.log('🔥 DB_HOST at runtime =', cfg.get('DB_HOST'));
+      console.log('🔥 ALL ENV =', process.env);
+
+      return {
         type: 'postgres',
         host: cfg.get('DB_HOST'),
         port: Number(cfg.get('DB_PORT')),
         username: cfg.get('DB_USER'),
         password: cfg.get('DB_PASSWORD'),
         database: cfg.get('DB_NAME'),
+        ssl: { rejectUnauthorized: false },
         autoLoadEntities: true,
         synchronize: true,
-        retryAttempts: 10,
-        retryDelay: 3000,
-      }),
+      };
+    },
     }),
   ],
 })
 export class DatabaseModule {}
+
